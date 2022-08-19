46b6a6d2f05a57185fe4b423e7d1d137b936861b
Nicolas Norena-Acosta <Nicolas.Norena@jhuapl.edu>
2022-08-18 21:46:51 -0400

    updated statistic_gui.py with support for rtabmap processing stats

--- scripts/statistics_visualizer_node.py
+++ scripts/statistics_visualizer_node.py
@@ -13,6 +13,7 @@ import rospy
 import sys
 import os
 import threading
+import time
 
 import numpy as np
 
@@ -21,9 +22,10 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-from stats_gui.statistic_gui import StatisticsGUI
+from stats_gui.statistic_gui import StatisticsGUI, RtabmapStruct
 
 from rtabmap_ros.msg import MapManagerStats
+from rtabmap_ros.msg import Info
 
 ###
 class StatisticsVisualizer:
@@ -50,8 +52,14 @@ class StatisticsVisualizer:
     # self.threadNH.daemon = True
     # self.threadNH.start()
 
+    self.rtabmap_time_total_sec_buf =  np.array([], dtype=np.float64)
+    self.last_response_time = time.time()
+    self.rtabmap_stats = RtabmapStruct()
+
     # Subscriber
     rospy.Subscriber("rtabmap/map_manager_stats", MapManagerStats, callback=self.mapManagerStatsCallback, queue_size=1)
+    rospy.Subscriber("rtabmap/info", Info, callback=self.infoStatsCallback, queue_size=1)
+
 
   def processingThread(self, stop):
     """
@@ -101,13 +109,53 @@ class StatisticsVisualizer:
 
     # update data in GUI
     if not msg.is_octomap_data:
+      # update processing elapsed time with latest values
       self.gui.update_map_manager_graph_2(mm_time, 0.0)
     else:
-      # graph times elapsed
+      # update processing elapsed time with latest values
       self.gui.update_map_manager_graph_2(mm_time, octomap_time)
-      # data display
-      self.gui.update_map_manager_data(grd_height)
+      # update ground plot with latest value
+      self.gui.update_map_manager_ground_graph(grd_height)
 
+
+  def infoStatsCallback(self, msg):
+    """
+    """
+    statsKeys = msg.statsKeys
+    statsValues = msg.statsValues
+
+    data_dictionary = {statsKeys[i]: statsValues[i] for i in range(len(statsKeys))}
+
+    rtabmap_time_total_sec = 0.
+    rtabmap_time_total_name = "RtabmapROS/TimeTotal/ms"
+    if rtabmap_time_total_name in data_dictionary.keys():
+      rtabmap_time_total_sec = data_dictionary[rtabmap_time_total_name]*(1/1000)
+
+    rtabmap_time_sec = 0.
+    rtabmap_time_name = "RtabmapROS/TimeRtabmap/ms"
+    if rtabmap_time_name in data_dictionary.keys():
+      rtabmap_time_sec = data_dictionary[rtabmap_time_name]*(1/1000)
+
+    # update rtabmap processing graph  
+    self.gui.update_rtabmap_graph(rtabmap_time_total_sec, rtabmap_time_sec)
+    
+    # update rtabmap stat data
+
+    #  calculate Hz responce within a 1 second of data
+    # in seconds
+    current_time = time.time()
+    time_elapsed = current_time - self.last_response_time
+    self.rtabmap_time_total_sec_buf = np.append(self.rtabmap_time_total_sec_buf, rtabmap_time_total_sec)
+
+    if time_elapsed >= 1:
+      # on seconds has passed
+      self.last_response_time = current_time
+      self.rtabmap_stats.rtabmap_hz = 1. / np.mean(self.rtabmap_time_total_sec_buf)
+      self.rtabmap_time_total_sec_buf = np.array([], dtype=np.float64)
+
+    self.gui.update_rtabmap_stat(self.rtabmap_stats)
+
+    
 # main function
 if __name__ == '__main__':
   """
--- scripts/stats_gui/statistic_gui.py
+++ scripts/stats_gui/statistic_gui.py
@@ -14,6 +14,12 @@ from PyQt5.QtWidgets import *
 
 from .matplotlib_qtwidget import MatplotlibWidget
 
+
+class RtabmapStruct():
+  def __init__(self):
+    self.rtabmap_hz = 0
+
+
 class StatisticsGUI(QWidget):
 
   def __init__(self):
@@ -30,33 +36,77 @@ class StatisticsGUI(QWidget):
     inner_box.setStyleSheet("QGroupBox {background-color: darkgrey; margin-top:-1em;}")
 
     inner_layout = QGridLayout()
+    # processing plot
     labels_names = ["total", "octomap"]
     self.mm_plot = MatplotlibWidget(width=5, height=4, dpi=100, title="Time Elapsed for Map Manager", multiple_line=True,
         label_names=labels_names)
-    self.value_octomap_grd_height = QDoubleSpinBox()
+    self.mm_plot_grd = MatplotlibWidget(width=5, height=4, dpi=100, title="Ground Height Over Time", y_axis_name="height [m]")
+
+    # data display
+    #self.value_octomap_grd_height = QDoubleSpinBox()
     
-    # adding widgets in inner box 
+    # inner box Widget: adding plot processing
     inner_layout.addWidget(self.mm_plot, 0, 0, 1, 2)
+
+    # inner box Widget: adding plot ground over time
     row_pos=1
-    label_octomap_grd_height = QLabel("Octomap ground height:")
-    label_octomap_grd_height.setAlignment(Qt.AlignRight)
-    self.value_octomap_grd_height.setSuffix(" meters")
-    self.value_octomap_grd_height.setDecimals(4)
-    self.value_octomap_grd_height.setButtonSymbols(QAbstractSpinBox.NoButtons)
-    self.value_octomap_grd_height.setMinimum(-50.0)
-    inner_layout.addWidget(label_octomap_grd_height, row_pos, 0, 1, 1)
-    inner_layout.addWidget(self.value_octomap_grd_height, row_pos,1 , 1, 1)
+    inner_layout.addWidget(self.mm_plot_grd, row_pos, 0, 1, 2)
+
+    # row_pos +=1
+    # label_octomap_grd_height = QLabel("Octomap ground height:")
+    # label_octomap_grd_height.setAlignment(Qt.AlignRight)
+    # self.value_octomap_grd_height.setSuffix(" meters")
+    # self.value_octomap_grd_height.setDecimals(4)
+    # self.value_octomap_grd_height.setButtonSymbols(QAbstractSpinBox.NoButtons)
+    # self.value_octomap_grd_height.setMinimum(-50.0)
+    # inner_layout.addWidget(label_octomap_grd_height, row_pos, 0, 1, 1)
+    # inner_layout.addWidget(self.value_octomap_grd_height, row_pos, 1 , 1, 1)
     # other data infomation TODO: include other data
-    row_pos +=1
+    # row_pos +=1
     # inner_layout.addWidget(label1, row_pos, 0, 1, 1)
     # inner_layout.addWidget(value1, row_pos, 0, 1, 1)
 
     # updates layout 
     inner_box.setLayout(inner_layout)
 
+    # --------------------------------------------------------
+    
+    # second tab
+    # inner box in tab rtabmap 
+    inner_box_2 = QGroupBox()
+    inner_box_2.setStyleSheet("QGroupBox {background-color: darkgrey; margin-top:-1em;}")
+
+    inner_layout_2 = QGridLayout()
+    # processing plot
+    labels_names = ["total", "rtabmap"]
+    self.rtabmap_plot = MatplotlibWidget(width=5, height=4, dpi=100, title="Rtabmap Processing", multiple_line=True,
+        label_names=labels_names)
+    # stats data
+    # Rtabmap_stats_hz
+    self.rtabmap_stats_hz = QDoubleSpinBox()
+    rtabmap_stats_hz_label = QLabel("Rtabmap Average Rate:")
+    rtabmap_stats_hz_label.setAlignment(Qt.AlignRight)
+    self.rtabmap_stats_hz.setSuffix(" Hz")
+    self.rtabmap_stats_hz.setDecimals(2)
+    self.rtabmap_stats_hz.setButtonSymbols(QAbstractSpinBox.NoButtons)
+
+    # inner box Widget: adding rtabmap processing plot
+    inner_layout_2.addWidget(self.rtabmap_plot, 0, 0, 1, 2)
+    #  stats data
+    # Rtabmap_stats_hz
+    row_pos=1
+    inner_layout_2.addWidget(rtabmap_stats_hz_label, row_pos, 0, 1, 1)
+    inner_layout_2.addWidget(self.rtabmap_stats_hz, row_pos, 1, 1, 1)
+
+    # updates layout 2
+    inner_box_2.setLayout(inner_layout_2)
+
+    # -----------------------------------------------------------
+
     # adding tab to main layout
     tabwidget = QTabWidget()
     tabwidget.addTab(inner_box, "map manager")
+    tabwidget.addTab(inner_box_2, "Rtabmap Stats")
     main_layout.addWidget(tabwidget, 0, 0)
 
     self.setLayout(main_layout)
@@ -68,14 +118,29 @@ class StatisticsGUI(QWidget):
   def update_map_manager_graph(self, updateTimedValue):
     """
     """
-    self.mm_plot.update_canvas(updateTimedValue)
+    self.mm_plot.update_canvas(updateTimedValue, set_y_limit_mode='time')
 
   def update_map_manager_graph_2(self, mm_update_timed_value, om_update_timed_value):
     """
     """
     self.mm_plot.update_canvas_2(mm_update_timed_value, om_update_timed_value)
 
-  def update_map_manager_data(self, value_ground_reference):
+  def update_map_manager_ground_graph(self, value_ground_reference):
     """
     """
-    self.value_octomap_grd_height.setValue(value_ground_reference)
+    self.mm_plot_grd.update_canvas(value_ground_reference, set_y_limit_mode='ground')
+
+  # def update_map_manager_data(self, value_ground_reference):
+  #   """
+  #   """
+  #   self.value_octomap_grd_height.setValue(value_ground_reference)
+
+  def update_rtabmap_graph(self, rtabmap_time_total_sec, rtabmap_time_sec):
+    """
+    """
+    self.rtabmap_plot.update_canvas_2(rtabmap_time_total_sec, rtabmap_time_sec, y_axes_lim_offset=0.1)
+
+  def update_rtabmap_stat(self, stats_data=RtabmapStruct()):
+    """
+    """
+    self.rtabmap_stats_hz.setValue(stats_data.rtabmap_hz)
